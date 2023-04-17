cd5f7640fc477bd9edc98c10dc17012247e43254
Vladislav Reznik <vladek_1999@mail.ru>
2023-04-17 14:21:55 +0300

    add test model and view

---
+++ CrocodileChallenge/Data/Model/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestModel.swift
+//  CrocodileChallenge
+//
+//  Created by Владислав Резник on 17.04.2023.
+//
+
+import Foundation
---
+++ CrocodileChallenge/Views/TestView.swift
@@ -0,0 +1,8 @@
+//
+//  TestView.swift
+//  CrocodileChallenge
+//
+//  Created by Владислав Резник on 17.04.2023.
+//
+
+import Foundation
