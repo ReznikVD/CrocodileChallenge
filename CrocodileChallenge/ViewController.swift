5261dbf9957dc9a719c5f7223b661177f563c56b
Vladislav Reznik <vladek_1999@mail.ru>
2023-04-17 12:48:50 +0300

    Initial Commit

---
+++ CrocodileChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrocodileChallenge
+//
+//  Created by Владислав Резник on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
