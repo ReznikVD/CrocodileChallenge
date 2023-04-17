df84d839d5e1825077ec2794741cd87f1fa6da36
iosDevelopForYou <iosdevelopforyou@gmail.com>
2023-04-17 21:07:27 +0400

    добавил background, тестовый коммит

--- CrocodileChallenge/ViewControllers/TeamViewController.swift
+++ CrocodileChallenge/ViewControllers/TeamViewController.swift
@@ -9,7 +9,28 @@ import UIKit
 
 class TeamViewController: UIViewController {
 
+    
+    private let backgroundImage: UIImageView = {
+        
+       let image = UIImageView(image: UIImage(named: "background"))
+        image.contentMode = .scaleAspectFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    func addviewLayout() {
+        view.addSubview(backgroundImage)
+        view.sendSubviewToBack(backgroundImage)
+        
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        addviewLayout()
     }
 }
