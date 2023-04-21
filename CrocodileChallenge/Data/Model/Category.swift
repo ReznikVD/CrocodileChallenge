1a6c1bc6a4e616efa6ca7e4474dc409a0e0eccf6
Sosisya <khaj-luizkin@yandex.ru>
2023-04-21 06:07:01 +0300

    preparation for adding words

--- CrocodileChallenge/Data/Model/Category.swift
+++ CrocodileChallenge/Data/Model/Category.swift
@@ -18,6 +18,37 @@ struct Category {
             Category(avatar: Resources.Images.burger, title: "Еда", color: Resources.Colors.lightGreen),
             Category(avatar: Resources.Images.cowboy, title: "Личности", color: Resources.Colors.blue),
             Category(avatar: Resources.Images.nails, title: "Хобби", color: Resources.Colors.red)
-            ]
+        ]
+    }
+
+    enum typeCategory: String {
+        case animal = "Животные"
+        case food = "Еда"
+        case persons = "Личности"
+        case hobby = "Хобби"
+
+    }
+
+    struct Words {
+        var category: Category
+
+        init(category: Category) {
+            self.category = category
+        }
+
+        func getWords() -> [String] {
+            guard let type = typeCategory(rawValue: category.title) else { return [] }
+            switch type {
+
+            case .animal:
+                return [""]
+            case .food:
+                return [""]
+            case .persons:
+                return [""]
+            case .hobby:
+                return [""]
+            }
+        }
     }
 }
